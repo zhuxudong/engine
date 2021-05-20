e958f5411ac8d09d4b049bfc7658b4809eab3b97
GuoLei1990 <gl3336563@163.com>
2021-05-20 15:52:11 +0800

    refactor: optimization code

--- packages/loader/src/gltf/GLTFParser.ts
+++ packages/loader/src/gltf/GLTFParser.ts
@@ -11,15 +11,30 @@ import { TextureParser } from "./parser/TextureParser";
 import { Validator } from "./parser/Validator";
 
 export class GLTFParser {
+  static instance = new GLTFParser([
+    BufferParser,
+    Validator,
+    TextureParser,
+    MaterialParser,
+    MeshParser,
+    EntityParser,
+    SkinParser,
+    AnimationParser,
+    SceneParser
+  ]);
+
   private static _isPromise(value: any): boolean {
     return value && typeof value.then === "function";
   }
 
-  private _pipeline: Parser[] = [];
+  private _pipelinePasses: Parser[] = [];
 
-  constructor(pipeline: (new () => Parser)[]) {
-    pipeline.forEach((Pipe: new () => Parser, index: number) => {
-      this._pipeline[index] = new Pipe();
+  /**
+   * @private
+   */
+  constructor(passes: (new () => Parser)[]) {
+    passes.forEach((pass: new () => Parser, index: number) => {
+      this._pipelinePasses[index] = new pass();
     });
   }
 
@@ -27,7 +42,7 @@ export class GLTFParser {
     let lastPipeOutput: void | Promise<void> = void 0;
 
     return new Promise((resolve, reject) => {
-      this._pipeline.forEach((parser: Parser) => {
+      this._pipelinePasses.forEach((parser: Parser) => {
         if (GLTFParser._isPromise(lastPipeOutput)) {
           lastPipeOutput = (lastPipeOutput as Promise<void>).then(() => {
             return parser.parse(context);
@@ -49,15 +64,3 @@ export class GLTFParser {
     });
   }
 }
-
-export const gltfParser = new GLTFParser([
-  BufferParser,
-  Validator,
-  TextureParser,
-  MaterialParser,
-  MeshParser,
-  EntityParser,
-  SkinParser,
-  AnimationParser,
-  SceneParser
-]);
